1ab81defeb8a9f81f26d7ba18255ca2f651891b4
Burnside1924 <tspencer1924@gmail.com>
2025-03-09 00:23:04 +0000

    Third commit with commenting style

---
+++ comments.js
@@ -0,0 +1,25 @@
+// create web server
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'));
+
+server.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var comments = [];
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.emit('init', comments);
+  socket.on('comment', function(msg){
+    comments.push(msg);
+    io.emit('comment', msg);
+  });
+});
